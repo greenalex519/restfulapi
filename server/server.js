1c65f7f10464f120f46016cd703c4a16f0da1cf5
David Parry <david.parry@suranyami.com>
2018-12-07 05:53:19 +1100

    initialise server

---
+++ server/server.js
@@ -0,0 +1,32 @@
+//BASE SETUP
+
+//call packages needed
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+//configure app to use bodyParser()
+//this allows us to get data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+
+//ROUTES FOR API
+
+const router = express.Router(); //get an instance of express router
+
+//test route to check is working
+router.get('/', function(req, res) {
+  res.json({ message: 'hurray! welcome to our API!' });
+});
+
+//more routes for API will happen here
+
+//REGISTER OUR ROUTES
+//all of our routes will be prefixed with '/api' 
+app.use('/api', router);
+
+//START THE SERVER
+app.listen(port);
+console.log('Magic happens on port ' + port);
