3f941fb9956266110ec58fb90d29cae84b259e6f
Alex Green <greenalex519@gmail.com>
2020-05-01 14:39:02 +1000

    creat POST at /api/rats

--- server/server.js
+++ server/server.js
@@ -3,10 +3,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const morgan = require('morgan');
-
-//configure app
-app.use(morgan('dev')); //logs request to console
 
 //configure app to use bodyParser()
 //this allows us to get data from a POST
@@ -34,7 +30,7 @@ const router = express.Router(); //get an instance of express router
 router.use(function(req, res, next) {
   console.log('Middleware reached.');
   next(); //make sure it then continues on its request to route...
-})
+});
 //middleware can later be added to confirm request is safe etc; user is authenticated; log data for analytics; collect statistic data etc...
 
 //test route to check is working
@@ -43,23 +39,22 @@ router.get('/', function(req, res) {
 });
 
 //on routes that end in /rats
-//-------------------------------------------
-
 router.route('/rats')
 
-  //create a rat (accessed at POST [...]/api/rats)
-  .post(function(req, res)
+  ///create a rat (accessed at POST localhost:8080/api/rats)
+  .post(function(req, res){
+    const rat = new Rat(); //create new instance of the rat model
+    rat.name = req.body.name; //set rats name via request
 
-    const rat = new Rat(); //create a new instance of rat model
-    rat.name = req.body.name; //assign name of rat with the data coming from the request
-
-    //save rat and check for errors
     rat.save(function(err) {
+
       if (err)
         res.send(err);
-
-      res.json({ message: 'Rat created!' });//implied else?
+      
+        res.json({ message: "Rat created!"});
     });
+
+  });
 
 
 //REGISTER OUR ROUTES
